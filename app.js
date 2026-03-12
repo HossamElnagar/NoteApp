df93a8e7d0df5a8e5c52c6ec4ee7ee724d8db888
HossamElnagar <Hossam7009@gmail.com>
2026-03-12 19:18:57 +0200

    first push

---
+++ app.js
@@ -0,0 +1,23 @@
+//dot env
+require("dotenv").config()
+//express
+const express = require("express")
+const app = express()
+//middleware JSON
+app.use(express.json())
+//port
+const port = process.env.PORT || 5000;
+//DB connection
+const mongoose = require("mongoose")
+async function dbconnection(){
+    try{
+    await mongoose.connect(process.env.DB_URL)
+    console.log("dbconnected")
+    }catch(err){
+        console.log(`server error`)
+    }
+}
+//runserver
+app.listen(port,()=>{
+    console.log(`the server is running at port${port}`)
+})
